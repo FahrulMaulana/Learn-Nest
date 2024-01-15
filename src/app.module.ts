41bf85077c6e94e096cfde74a0287bec69965e40
fahrul <fahruluyah12@gmail.com>
2024-01-15 11:00:32 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,14 @@ import { UserModule } from './user/user.module';
 import { CalonSiswaModule } from './calon_siswa/calon_siswa.module';
 import { JenisKelaminModule } from './jenis_kelamin/jenis_kelamin.module';
 import { PendaftaranModule } from './pendaftaran/pendaftaran.module';
+import { DataOrtuModule } from './data_ortu/data_ortu.module';
+import { PekerjaanAyahModule } from './pekerjaan_ayah/pekerjaan_ayah.module';
+import { PendidikanAyahModule } from './pendidikan_ayah/pendidikan_ayah.module';
+import { PekerjaanIbuModule } from './pekerjaan_ibu/pekerjaan_ibu.module';
+import { PendidikanIbuModule } from './pendidikan_ibu/pendidikan_ibu.module';
 
 @Module({
-  imports: [BooksModule, PrismaModule, UserModule, CalonSiswaModule, JenisKelaminModule, PendaftaranModule],
+  imports: [BooksModule, PrismaModule, UserModule, CalonSiswaModule, JenisKelaminModule, PendaftaranModule, DataOrtuModule, PekerjaanAyahModule, PendidikanAyahModule, PekerjaanIbuModule, PendidikanIbuModule],
   controllers: [AppController],
   providers: [AppService],
 })
