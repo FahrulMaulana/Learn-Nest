776b6757214c90abb55bc661fe467f87bdda6e30
fahrul <fahruluyah12@gmail.com>
2024-01-15 22:32:07 +0700

    add user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,8 @@
 /* eslint-disable prettier/prettier */
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { UserService } from './user.service';
 import { createUsersDTO } from 'src/books/dto/createuser.dto';
+import { loginDTO } from 'src/books/dto/login.dto';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
@@ -20,6 +21,12 @@ export class UserController {
     @Post()
     async createUser(@Body()payload: createUsersDTO){
         return await this.userService.createData(payload)
+    }
+
+    @Post('/login')
+    async login(@Body()Body: loginDTO){
+        const token =  await this.userService.login(Body.email,Body.password)
+        return {token}
     }
 
     // @Post()
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,28 +1,50 @@
 /* eslint-disable prettier/prettier */
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { AuthService } from 'src/auth/auth.service';
 import { createUsersDTO } from 'src/books/dto/createuser.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UserService {
-    constructor(private dbService: PrismaService){}
+    constructor(private dbService: PrismaService, private auth: AuthService){}
 
     async findAll(){
         return await this.dbService.user.findMany();
     }
 
     async createData(createUsersDTO: createUsersDTO){
-        const {email,name,role} = createUsersDTO
-         await this.dbService.user.createMany({
+        const defaulRole = "1"
+
+        const {email,name,username,password} = createUsersDTO
+        const emailRegister = await this.dbService.user.findUnique({
+            where : {
+                email
+            }
+        })
+
+        if (emailRegister) {
+            throw new ConflictException('email sudah terdaftar')
+        }
+
+        const hash= await bcrypt.hash(password, 10)
+        await this.dbService.user.createMany({
             data:{
             email,
             name,
-            role
+            id_role : defaulRole,
+            username,
+            password: hash
     }})
         const updateSuccessful = true; // Adjust this based on your actual logic
         return { success: updateSuccessful }
     }
 
+    async login(email:string, password:string){
+        return await this.auth.login(email,password)
+    }
+
+  
     // async createUser(data: any){
     //     return await this.dbService.user.create({
     //         data
